5b26707ad9b9c3d0c6589e1a8a63756814c08ccb
Subhranil <subhra750nil@gmail.com>
2024-08-15 22:20:57 +0530

    functionally working properly

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+      let myStyle = {
+            minHeight: "75vh",
+            margin: "40px auto"
+      }
+      return (
+            <div style={myStyle}>
+                  This is ABOUT component of this app.
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat distinctio nam laborum cum illum pariatur neque sequi culpa quas earum.</p>
+            </div>
+      )
+}
+
